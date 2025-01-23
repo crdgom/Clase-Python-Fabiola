f786c46cb36010e122f4d50c049bfa656dd84598
amatista-fa <fabiola.olvera.ac@gmail.com>
2025-01-23 07:32:04 -0600

    Update and rename 02_for_loop_Example.py to List_of_fruits_Fabz.py

---
+++ 1 Brush Up on Common Coding Constructs/01 Control Flow (Flujos)/02_Loops/List_of_fruits_Fabz.py
@@ -0,0 +1,10 @@
+def showme_fruits():#Cambiamos el nombre de la funcion para ser mas descriptivos
+        """
+        Basic example of a for loop:
+        Iterates through a list of fruits and prints them.
+        """
+        fruits_list = ["apple", "banana", "coco"] #Le cambiamos el nombre de platano a coco porque hace mas sentido que todo este en el mismo idioma
+        for fruit in fruits_list: #cambie el nombre de fruits a fruits_list para ser mas descritivo
+            print(f"The current fruit is: {fruit}")
+
+showme_fruits()
