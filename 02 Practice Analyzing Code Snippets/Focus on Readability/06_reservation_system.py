8b31a5f17b9906b29e9c912dcc62df96083ba445
amatista-fa <fabiola.olvera.ac@gmail.com>
2025-01-23 07:25:14 -0600

    Reservation_system_Fabz.py

--- 02 Practice Analyzing Code Snippets/Focus on Readability/06_reservation_system.py
+++ 02 Practice Analyzing Code Snippets/Focus on Readability/06_reservation_system.py
@@ -1,10 +1,12 @@
-class create_reservation:
+class Reservation: #Le cambiaria el nombre de create_reservation a Reservation 
     """Sistema básico de reservas para un restaurante."""
-    def __init__(self, name, guests, time):
+    def __init__(self, name, guests, time):#le agregaria el apellido como un argumento mas "last_name"
         self.name = name
-        self.guests = guests
-        self.time = time
-
+        self.guests = guests #le cambiaria el combre a la variable a guests_number 
+        self.time = time #Le cambiaria el nombre a "ckeckin_date_hr
+       #self.last_name= last_name
+       #Le agregaria un atributo llamado dia y hora de checkout_date_hr
+   
     def display_reservation(self):
         """Muestra los detalles de la reserva."""
         print(f"Reserva a nombre de {self.name} para {self.guests} personas a las {self.time}.")
